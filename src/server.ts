7dd158687344853659c1656891d709db141da429
Skyla Miranda Reyes <skylamiranda643@gmail.com>
2025-03-18 17:46:26 -0400

    opppaaa

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,16 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import productRoutes from "./routes/productRoutes";
+import ecoRoutes from "./routes/ecoRoutes";
+import supplyChainRoutes from "./routes/supplyChainRoutes";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-
+app.use("/api/products", productRoutes);
+app.use("/api/eco", ecoRoutes);
+app.use("/api/supply-chain", supplyChainRoutes);
 app.get('/', (req, res) => {
   res.send('Server is running!');
 });
