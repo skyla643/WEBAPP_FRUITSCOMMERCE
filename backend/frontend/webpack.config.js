3103b50491e6becc589fdd89628d7d610bdc3e98
Skyla Miranda Reyes <skylamiranda643@gmail.com>
2025-03-18 18:22:49 -0400

    add

--- backend/frontend/webpack.config.js
+++ backend/frontend/webpack.config.js
@@ -14,3 +14,19 @@ module.exports = {
     }
   }
 };
+module.exports = {
+    resolve: {
+      fallback: {
+        "zlib": require.resolve("browserify-zlib"),
+        "querystring": require.resolve("querystring-es3"),
+        "path": require.resolve("path-browserify"),
+        "crypto": require.resolve("crypto-browserify"),
+        "stream": require.resolve("stream-browserify"),
+        "fs": false, // Some modules depend on `fs`, but it's not available in the browser
+        "net": false, // Same with `net`
+        "http": require.resolve("stream-http"),
+        "https": require.resolve("https-browserify"),
+        "util": require.resolve("util"),
+      }
+    }
+  };
