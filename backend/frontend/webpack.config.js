715889b93bb54af96650a4196f743f69aeba4f9e
Skyla Miranda Reyes <skylamiranda643@gmail.com>
2025-03-18 18:27:00 -0400

    replace

--- backend/frontend/webpack.config.js
+++ backend/frontend/webpack.config.js
@@ -10,23 +10,12 @@ module.exports = {
       "stream": require.resolve("stream-browserify"),
       "util": require.resolve("util/"),
       "url": require.resolve("url/"),
-      "buffer": require.resolve("buffer/")
+      "buffer": require.resolve("buffer/"),
+      "fs": false, // fs is not available in the browser
+      "net": false, // net is not available in the browser
+      "http": require.resolve("stream-http"),
+      "https": require.resolve("https-browserify"),
+      "process": require.resolve("process/browser"),
     }
   }
 };
-module.exports = {
-    resolve: {
-      fallback: {
-        "zlib": require.resolve("browserify-zlib"),
-        "querystring": require.resolve("querystring-es3"),
-        "path": require.resolve("path-browserify"),
-        "crypto": require.resolve("crypto-browserify"),
-        "stream": require.resolve("stream-browserify"),
-        "fs": false, // Some modules depend on `fs`, but it's not available in the browser
-        "net": false, // Same with `net`
-        "http": require.resolve("stream-http"),
-        "https": require.resolve("https-browserify"),
-        "util": require.resolve("util"),
-      }
-    }
-  };
