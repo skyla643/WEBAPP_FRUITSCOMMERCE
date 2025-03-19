bc09ca3484b4feca0e886b58b7860e7a1482d728
Skyla Miranda Reyes <skylamiranda643@gmail.com>
2025-03-19 16:44:03 -0400

    Update App.tsx

--- backend/frontend/src/App.tsx
+++ backend/frontend/src/App.tsx
@@ -1,3 +1,4 @@
+import "./index.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Dashboard from "./components/Dashboard";
