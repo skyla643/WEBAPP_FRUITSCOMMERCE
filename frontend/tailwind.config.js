954462e754915ca9f15cc473e49cbe0ca76787b2
Skyla Miranda Reyes <skylamiranda643@gmail.com>
2025-03-19 17:23:27 -0400

    ??

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,7 +1,6 @@
 module.exports = {
   content: [
-    "./src/**/*.{js,ts,jsx,tsx}",
-    "./backend/frontend/src/**/*.{js,ts,jsx,tsx}"
+    "./**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
     extend: {},
