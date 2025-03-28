6ed6d262519d4c7a7ea2ee9b34f4f807765bbc9a
Skyla Miranda Reyes <skylamiranda643@gmail.com>
2025-03-28 16:07:12 -0400

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './components/Login';
-import Dashboard from '../../src/components/Dashboard';
+import Dashboard from './components/Dashboard';
 
 const App: React.FC = () => {
   return (
