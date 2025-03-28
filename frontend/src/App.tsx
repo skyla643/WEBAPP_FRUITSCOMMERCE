a4ee7cf800272111acf03d99688d1a11e5bb28e2
Skyla Miranda Reyes <skylamiranda643@gmail.com>
2025-03-28 15:58:44 -0400

    errror gone

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './components/Login';
-import Dashboard from './components/Dashboard';
+import Dashboard from '../../src/components/Dashboard';
 
 const App: React.FC = () => {
   return (
