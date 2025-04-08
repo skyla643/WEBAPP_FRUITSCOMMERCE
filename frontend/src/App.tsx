ed1e44d66c7ce962baa60392892dbd0b39e82ba4
Skyla Miranda Reyes <skylamiranda643@gmail.com>
2025-04-08 13:34:54 -0400

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,20 +2,20 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './components/Login';
+import Signup from './components/Signup';
 import Dashboard from './components/Dashboard';
 import Orchards from './components/Orchards';
 import PestDetection from './components/PestDetection';
 import SupplyChain from './components/SupplyChain';
 import MarketData from './components/MarketData';
-import ClientManager from './components/ClientManager'; // <-- ADDED
-import Signup from './components/Signup';
-<Route path="/signup" element={<Signup />} />
+import ClientManager from './components/ClientManager';
 
 const App: React.FC = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/orchards" element={<Orchards />} />
         <Route path="/pest-detection" element={<PestDetection />} />
