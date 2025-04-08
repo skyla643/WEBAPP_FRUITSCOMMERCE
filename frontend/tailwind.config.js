a9f648c68a611ad6ec2e7cfb46a994db877d42d2
Skyla Miranda Reyes <skylamiranda643@gmail.com>
2025-04-08 14:29:48 -0400

    Update tailwind.config.js

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -46,3 +46,13 @@ module.exports = {
     },
   }
 }
+module.exports = {
+  theme: {
+    extend: {
+      animation: {
+        'spin-slow': 'spin 4s linear infinite',
+      },
+    },
+  },
+  plugins: [],
+}
