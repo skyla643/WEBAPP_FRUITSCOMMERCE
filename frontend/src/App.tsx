d4ade17be93ea903ba622bfc7abfb1ec1957c22f
Skyla Miranda Reyes <skylamiranda643@gmail.com>
2025-04-10 11:30:23 -0400

    add

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,11 @@
-// frontend/src/App.tsx (Even More Simplified)
+// frontend/src/App.tsx (Minimal)
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
-import MapTest from './components/MapTest';
 
 const App: React.FC = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/map-test" element={<MapTest />} />
-      </Routes>
-    </Router>
+    <div>
+      <h1>Hello from App!</h1>
+    </div>
   );
 };
 
